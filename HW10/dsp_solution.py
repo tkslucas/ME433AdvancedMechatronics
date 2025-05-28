ae4b5b4403776c3fb1f92f3621acd103c4d856d1
tkslucas <takayaslucas@gmail.com>
2025-05-28 11:45:39 -0500

    HW10 done, small improvement, save images instead of displaying

--- HW10/dsp_solution.py
+++ HW10/dsp_solution.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 from coefficients import h_1, h_2, h_3, h_4
 
 def load_signal_data(address):
@@ -21,13 +22,13 @@ def calculate_fft(time, signal):
     frq = np.arange(n) / (n / Fs)
     return frq[:n//2], abs(Y[:n//2])
 
-def plot_results(raw_time, raw_signal, filtered_time, filtered_signal, filter_name, params):
+def plot_results(raw_time, raw_signal, filtered_time, filtered_signal, filter_name, params, signal_name):
     # Calculate FFTs
     raw_frq, raw_Y = calculate_fft(raw_time, raw_signal)
     filt_frq, filt_Y = calculate_fft(filtered_time, filtered_signal)
     
     # Create plots
-    fig, (ax1, ax2) = plt.subplots(2, 1)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
     
     ax1.set_xlabel('Time')
     ax1.set_ylabel('Amplitude')
@@ -41,7 +42,13 @@ def plot_results(raw_time, raw_signal, filtered_time, filtered_signal, filter_na
 
     ax1.legend()
     ax2.legend()
-    plt.show()
+
+    # Sanitize filename and save
+    os.makedirs("output_plots", exist_ok=True)
+    safe_params = str(params).replace(" ", "_").replace(",", "_").replace("=", "-").replace("(", "").replace(")", "")
+    filename = f"output_plots/{signal_name}_{filter_name}_{safe_params}.png"
+    plt.savefig(filename)
+    plt.close(fig)
 
 def MAF(address, window_size):
     time, signal = load_signal_data(address)
@@ -52,7 +59,8 @@ def MAF(address, window_size):
         filtered_signal.append(sum(window) / window_size)
     
     filtered_time = np.linspace(time[0], time[-1], len(filtered_signal))
-    plot_results(time, signal, filtered_time, filtered_signal, "MAF", f"window={window_size}")
+    signal_name = os.path.splitext(os.path.basename(address))[0]
+    plot_results(time, signal, filtered_time, filtered_signal, "MAF", f"window={window_size}", signal_name)
 
 def IIR(address, weight_A, weight_B):
     if not np.isclose(weight_A + weight_B, 1.0):
@@ -66,7 +74,8 @@ def IIR(address, weight_A, weight_B):
     for i in range(1, len(signal)):
         filtered_signal[i] = weight_A * filtered_signal[i-1] + weight_B * signal[i]
     
-    plot_results(time, signal, time, filtered_signal, "IIR", f"A={weight_A}, B={weight_B}")
+    signal_name = os.path.splitext(os.path.basename(address))[0]
+    plot_results(time, signal, time, filtered_signal, "IIR", f"A={weight_A}, B={weight_B}", signal_name)
 
 def FIR(address, coefficients, method):
     time, signal = load_signal_data(address)
@@ -77,7 +86,8 @@ def FIR(address, coefficients, method):
         filtered_signal.append(np.dot(coefficients, window))
     
     filtered_time = np.linspace(time[0], time[-1], len(filtered_signal))
-    plot_results(time, signal, filtered_time, filtered_signal, "FIR", f"{method} ({len(coefficients)} taps)")
+    signal_name = os.path.splitext(os.path.basename(address))[0]
+    plot_results(time, signal, filtered_time, filtered_signal, "FIR", f"{method} ({len(coefficients)} taps)", signal_name)
 
 # Signal processing paths and parameters
 SIGNAL_PATHS = [
